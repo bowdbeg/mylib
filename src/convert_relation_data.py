28f694a0fdc796a2a20df091195c09ea903af2d0
m11k08 <bear.kohei@gmail.com>
2020-09-01 18:23:45 +0900

    update

---
+++ src/convert_relation_data.py
@@ -0,0 +1,70 @@
+from relation_data import RelationData
+from pathlib import Path
+from argparse import ArgumentParser
+
+TYPES = ['ann', 'docred']
+EXTS = {'ann': 'ann', 'docred': 'json'}
+
+
+def infer_type(path):
+    path = Path(path)
+
+    if path.is_dir():
+        files = path.glob('*.{}'.format(ext))
+        out = None
+        for tp in TYPES:
+            ext = EXTS[tp]
+            if any([ext in f.suffix for f in files]):
+                assert out is None
+                out = tp
+    else:
+        out = None
+        for tp in TYPES:
+            ext = EXTS[tp]
+            if ext in path.suffix:
+                assert out is None
+                out = tp
+
+    return out
+
+
+def convert(args):
+    input = args.input
+    output = args.output
+    output_type = args.output_type
+    input_type = args.input_type
+    spacy_model = args.spacy
+
+    if input_type == 'auto':
+        input_type = infer_type(input)
+        if input_type is None:
+            raise ValueError('Cannot infer type from your input.')
+
+    if input_type == 'ann':
+        pattern = '*.ann'
+    else:
+        raise NotImplementedError
+
+    if output_type == 'docred':
+        data = RelationData(input,
+                            pattern=pattern,
+                            data_type=input_type,
+                            spacy=spacy_model)
+        data.export_docred(output, spacy=spacy_model)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument('input', type=Path)
+    parser.add_argument('output', type=Path)
+    parser.add_argument('output_type', type=str, choices=TYPES)
+    parser.add_argument('--input_type',
+                        type=Path,
+                        choices=['auto', *TYPES],
+                        default='auto')
+    parser.add_argument('--spacy', type=str, default='en_core_sci_sm')
+
+    args = parser.parse_args()
+
+    convert(args)
--- src/relation_data.py
+++ src/relation_data.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import spacy
+import json
 
 
 class RelationDatum():
@@ -10,35 +12,33 @@ class RelationDatum():
         else:
             self.data_type = data_type
 
-        self.parse()
+        self.data = self.parse()
 
     def parse(self):
         return self.parse(self.data_path, data_type=self.data_type)
 
     def parse(self, data_path, data_type='auto'):
-        data_path  = Path(data_path)
+        data_path = Path(data_path)
         if data_type == 'auto':
             data_type = data_path.suffix[1:]
-
 
         if data_type == 'ann':
             data = self.parse_ann(data_path)
         else:
             raise NotImplementedError
-        
+
         self.data = data
         return data
 
     @staticmethod
     def parse_ann(ann_path):
         ann_path = Path(data_path)
-        txt_path = data_path.parent/ "{}.txt".format(ann_path.stem)
+        txt_path = data_path.parent / "{}.txt".format(ann_path.stem)
 
-        if not(ann_path.exists() and txt_path.exists()):
+        if not (ann_path.exists() and txt_path.exists()):
             raise FileNotFoundError
 
         ann_loader = AnnDataLoader()
-
 
         data = {}
 
@@ -50,9 +50,108 @@ class RelationDatum():
         data['event'] = events
 
         return data
-    
-    def export_docred(self):
 
+    def export_docred(self, ofile, **kwargs):
+        otxt = self.export_docred(**kwargs)
+        with open(ofile, 'w') as f:
+            f.write(otxt)
+
+        return otxt
+
+    def export_docred(self, *args, **kwargs):
+        dic = self.export_docred_as_dict(*args, **kwargs)
+        otxt = json.dumps(dic)
+        return otxt
+
+    def export_docred_as_dict(self, spacy='en_core_sci_sm'):
+        assert self.data
+        nlp = spacy.load(spacy)
+
+        dic = {}
+        dic['title'] = data['id']
+        dic['vertexSet'] = []
+        dic['labels'] = []
+
+        doc = nlp(data['text'])
+
+        sents = [list(map(lambda x: x.text, s)) for s in doc.sents]
+        dic['sents'] = sents
+
+        # get span of word
+        for ent in self.data['entity'].values():
+            start = ent['start']
+            end = ent['end']
+            label = ent['label']
+            entity = ent['entity']
+
+            wstart = None
+            wend = None
+            for sid, sent in enumerate(doc.sents):
+                for wid, token in enumerate(sent):
+                    if start > token.idx and wstart is None:
+                        wstart = wid - 1
+                    if end > token.idx and wend is None:
+                        assert wstart is not None
+                        wend = wid
+                        mention = sent[wstart:wend].text
+                    if wstart is not None and wend is not None:
+                        sent_id = sid
+                        break
+                if wstart is not None and wend is not None:
+                    break
+
+            dic['vertexSet'].append([{
+                'name': mention,
+                'send_id': sent_id,
+                'pos': [wstart, wend],
+                'type': label
+            }])
+
+        # define relation
+        keys = list(data['entity'].keys())
+
+        for rel in data['relation'].values():
+            rel_label = rel['label']
+            arg1 = rel['arg1']
+            arg2 = rel['arg2']
+
+            eid1 = keys.index(arg1)
+            eid2 = keys.index(arg2)
+
+            dic['labels'].append({'h': eid1, 't': eid2, 'r': rel})
+
+        return dic
+
+
+class RelationData():
+    def __init__(self,
+                 dir_path,
+                 pattern='*',
+                 data_type='auto',
+                 spacy='en_core_sci_sm'):
+        self.dir_path = Path(dir_path)
+        self.pattern = pattern
+        files = self.dir_path.glob(pattern)
+
+        self.data = {}
+
+        for f in files:
+            self.data[f] = RelationDatum(f)
+
+    def export_docred(self, spacy='en_core_sci_sm'):
+        odir = Path(ofile)
+        odata = [
+            v.export_docred_as_dict(spacy=spacy) for v in self.data.values()
+        ]
+
+        otxt = json.dumps(odata)
+        return otxt
+
+    def export_docred(self, ofile, **kwargs):
+        ofile = Path(ofile)
+        otxt = self.export_docred(**kwargs)
+        ofile.write_text(otxt)
+        return otxt
 
 
 # ann data loader
