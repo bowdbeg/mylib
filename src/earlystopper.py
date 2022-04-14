acc8fd5bc85176fff2b094f2660fec6a168cac65
m11k08 <bear.kohei@gmail.com>
2022-04-14 02:42:53 +0000

    update early stopping

--- src/earlystopper.py
+++ src/earlystopper.py
@@ -1,40 +1,32 @@
-from copy import deepcopy
+import numpy as np
+import torch
+from pathlib import Path
+import dataclasses
 
 
-class EarlyStopper():
-    def __init__(self,
-                 model,
-                 patience=10,
-                 threshold=1e-3,
-                 startup=0,
-                 mode='min'):
-        assert mode in ['min', 'max']
-        self.patience = patience
-        self.threshold = threshold
-        self.startup = startup
-        self.values = []
-        self.mode = mode
-        if self.mode == 'min':
-            self.sign = 1.
-        elif self.mode == 'max':
-            self.sign = -1.
-        self.count = 0
-        self.model = model
-        self.best_param = deepcopy(model.state_dict())
-        self.best_epoch = 0
+@dataclasses.dataclass
+class EarlyStopper:
+    patience: int
+    delta: float = 0.0
+    path: Path = Path("checkpoint.pt")
 
-    def step(self, val):
-        if len(self.values) == 0:
-            self.values.append(val)
-            return False
-        val_past = self.values[-1]
-        sub = val - val_past
-        self.values.append(val)
-        if self.sign * sub > 0 or abs(sub) < self.threshold:
-            self.count += 1
+    def __post_init__(self) -> None:
+        self.coutner = 0
+        self.best_score = -float("inf")
+        self.early_stop = False
+        self.patience = self.patience if self.patience else float("inf")
+
+    def __call__(self, score, model) -> bool:
+
+        if score < self.best_score + self.delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
         else:
-            self.count = 0
-            self.best_param = deepcopy(self.model.state_dict())
-            self.best_epoch = len(self.values)
+            self.best_score = score
+            self.save_checkpoint(model)
+            self.counter = 0
+        return self.early_stop
 
-        return self.count > self.patience and len(self.values) > self.startup
+    def save_checkpoint(self, model):
+        torch.save(model.state_dict(), self.path)
